427d311c1f3be237f4a637a873ee1090012eda2d
kvk55247 <kurapothulavamsi@gmail.com>
2025-11-17 11:05:11 +0530

    jenkins

---
+++ vars/nodejsEKSPipeline.groovy
@@ -0,0 +1,148 @@
+def call(Map configMap){
+    pipeline {
+        agent {
+                node {
+                    label 'AGENT-1'
+                }
+            }
+        environment {
+            appVersion = ''
+            REGION = "us-east-1"
+            ACC_ID = "784585544641"
+            PROJECT = "roboshop"
+            COMPONENT = "catalogue"
+
+        }
+        options {
+            timeout(time: 30, unit: 'MINUTES') 
+            disableConcurrentBuilds()
+        }
+        parameters {
+            booleanParam(name: 'deploy', defaultValue: true, description: 'Toggle this value')
+        }
+    // build
+        stages {
+            stage('Read package.json') {
+                steps {
+                    script {
+                        def packageJson = readJSON file: 'package.json'
+                        appVersion = packageJson.version
+                        echo "Project Version: ${appVersion}"
+                    }
+                }
+            }
+            stage('Install Dependencies') {
+                steps {
+                    script {
+                        sh """
+                            npm install
+                        """
+                    }
+                }
+            }
+            stage('Unit Testing') {
+                steps {
+                    script {
+                        sh """
+                            echo "unit testing"
+                        """
+                    }
+                }
+            }
+           /*  stage('sonar scan') {
+                environment {
+                    scannerHome = tool 'sonar-7.2'
+                }
+                steps {
+                    script {
+                        withSonarQubeEnv('sonar-7.2') {
+                        sh "${scannerHome}/bin/sonar-scanner"
+                        }
+                    }
+                }
+            } */
+            // enable webhook in sonarqube server
+          /*   stage("Quality Gate") {
+                steps {
+                    timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: true }
+                }
+            } */
+           /*  stage('Check Dependabot Alerts') {
+                steps {
+                    script {
+                        // Call the GitHub API
+                        def response = sh(
+                            script: """
+                                curl -s -H "Accept: application/vnd.github+json" \
+                                    -H "Authorization: token ${GITHUB_TOKEN}" \
+                                    https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/dependabot/alerts
+                            """,
+                            returnStdout: true
+                        ).trim() */
+
+                        // Parse JSON using Groovy's JsonSlurper
+                       /*  def json = new groovy.json.JsonSlurper().parseText(response)
+
+                        // Filter for high or critical severity
+                        def criticalOrHigh = json.findAll { it.security_advisory.severity in ['high', 'critical'] }
+
+                        if (criticalOrHigh.size() > 0) {
+                            echo "Found ${criticalOrHigh.size()} critical/high Dependabot alerts:"
+                            criticalOrHigh.each { alert ->
+                                echo "- ${alert.security_advisory.summary} (Severity: ${alert.security_advisory.severity})"
+                            }
+                            error("Pipeline failed due to critical/high Dependabot alerts!")
+                        } else {
+                            echo "No critical or high Dependabot alerts found."
+                        }
+                    }
+                }
+            } */
+            stage('Docker Build') {
+                steps {
+                    script { 
+                        withAWS(credentials: 'aws-creds', region: 'us-east-1') {
+                            sh """  
+                                aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} 
+                            """
+                        }    
+                    }
+                }
+            }
+            stage('trigger deploy') {
+                when {
+                    expression { params.deploy }
+                }
+                steps {
+                    script { 
+                        build job: 'catalogue-deploy',
+                        parameters: [
+                            string(name: 'appVersion', value: "${appVersion}"),
+                            string(name: 'deploy_to', value: 'dev')
+                        ],
+                        wait: false,// vpc will not wait for sg pipeline completion
+                        propagate: false // even sg fails vpc will not be affected
+                            
+                    }    
+                }
+            }
+        }
+    }        
+    post { 
+        always { 
+            echo 'I will always say Hello again!'
+        }
+        success {
+            echo 'hi this is success'
+            deleteDir()
+        }
+        failure {
+            echo 'hi, this is failure'
+        }
+    }
+}
+                
+            
